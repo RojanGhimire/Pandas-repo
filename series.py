3bfeb1b081321c27b09491e6ea17add56db23c1d
RojanGhimire <ghimirerojan41@gmail.com>
2026-01-16 15:56:45 +0545

    completed series pandas

---
+++ series.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+#series : a panda 1 dimensional array that can hold any type of data
+#think of it as a single column spreadsheet
+
+list = pd.Series(["charmander","charmeleon","charizard"],index=['a','b','c'])    #the index can also be selected in the series
+list.loc['a']= "bulbasaur"     #loc locate function can be used to locate or print or change the contents of series in certain indexes
+
+# print(list)
+
+studyhour  = {"sunday":12,"monday":11,"tuesday":5,"wednesday":2}
+series = pd.Series(studyhour)
+# print(series.loc["monday"])
+print(series[series >= 6])
+
+
+
